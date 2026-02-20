14c5f8b95d98f1fca9241af44059f5830c8cdc38
Dipput <dipalit26@gmail.com>
2026-02-20 13:04:39 +0530

    new class created

---
+++ src/test/java/Automationpractice/AmazonMarathi.java
@@ -0,0 +1,27 @@
+package Automationpractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AmazonMarathi {
+
+	@Test
+	
+	public void Marathi()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://www.amazon.in/");
+		WebElement click = driver.findElement(By.xpath("//div[@id='nav-flyout-icp']//span[text()='मराठी']/preceding-sibling::i[@class='icp-radio']"));
+		 driver.quit();
+		
+		
+	}
+}
